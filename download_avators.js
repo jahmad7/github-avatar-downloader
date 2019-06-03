02775faa3c9c23c5dcc1bb11cab03b9130d45d9e
Junaid <jahmad095@gmail.com>
2019-06-03 17:06:19 -0400

    added functions

--- download_avators.js
+++ download_avators.js
@@ -1,4 +1,23 @@
+//Junaid Ahmad 
+//Git Hub Avator downloader 
+//application used to download the avators of github users based on in command line provided username and repository
+//for example node download_avators.js nodejs node
+
+
 var request = require("request");
 var fs = require ("fs");
 
-console.log("hello");
+
+//use the request module to GET the list of contrubtors for the given repo
+function getRepoContributors(repoOwner, repoName, callback){
+    
+}
+
+
+//run the function get the list we need 
+getRepoContributors("jquery", "jquery", function(err, result){
+    if (err){
+        console.log(err);
+    }
+    console.log(result);
+})
