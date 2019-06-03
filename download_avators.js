73c0061d48d6d04bee97329ba8ab18fef3551aa1
Junaid <jahmad095@gmail.com>
2019-06-03 17:45:27 -0400

    learned how to use JSON

--- download_avators.js
+++ download_avators.js
@@ -6,6 +6,7 @@
 
 var request = require("request");
 var fs = require ("fs");
+var gitInfrmation = require("./noncommit");
 
 
 //use the request module to GET the list of contrubtors for the given repo
@@ -18,20 +19,29 @@ function getRepoContributors(repoOwner, repoName, callback){
         //case sensative information passed into the request header
         headers: {
             'User-Agent': 'request',
-            'Authorization': 'token 854d9c9f72a050ef7838f09cebdc7c194064f057'
+            'Authorization': gitInfrmation.GITHUB_TOKEN
         }
     };
 
     request(options, function(error, response, body){
-        callback(JSON.parse(body));
+        if (error){
+            console.log(error)
+        }
+        else if (response.statusCode === 200){
+            callback(error, JSON.parse(body));
+        }
     });
 }
 
 
 //run the function get the list we need 
 getRepoContributors("jquery", "jquery", function(err, result){
+    //make sure there are no errors being thrown
     if (err){
         console.log(err);
     }
-    console.log(err, result);
+    //iterate through the object to get the values we want 
+    for(var user of result){
+        console.log(user.avatar_url);
+    }
 })
